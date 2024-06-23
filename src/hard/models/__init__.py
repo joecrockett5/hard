81c3d448e78df50a12bb28bcc2bb3f38262e79b5
joecrockett5 <joe@crockett.net>
2024-06-23 17:06:23 +0100

    Import models in __init__.py for easier usage

--- src/hard/models/__init__.py
+++ src/hard/models/__init__.py
@@ -4,3 +4,10 @@ from enum import Enum
 class WeightUnit(Enum):
     KILOGRAMS = "kgs"
     POUNDS = "lbs"
+
+
+from .exercise_template import ExerciseTemplate
+from .exercise import Exercise
+from .set import Set
+from .tag import Tag
+from .workout import Workout
