5af8163fd3248bb756ee498a4b62c068c0e6e942
joecrockett5 <joe@crockett.net>
2024-06-30 15:03:59 +0100

    Add validation to and from DB

--- src/hard/aws/dynamodb/base_object.py
+++ src/hard/aws/dynamodb/base_object.py
@@ -1,9 +1,12 @@
 from pydantic import BaseModel, field_validator, field_serializer
+from pydantic_core import PydanticCustomError
 from datetime import datetime
 from dateutil import parser as date_parser
 
 from hard.aws.dynamodb.object_type import ObjectType
 
+DB_PARTITION = "User_ObjectType"
+DELIMITER = "_"
 
 class BaseObject(BaseModel):
     """Base object for all items stored in DynamoDB"""
@@ -31,3 +34,28 @@ class BaseObject(BaseModel):
     @field_serializer("object_type")
     def get_value(object_type: ObjectType):
         return object_type.value
+    
+    def to_db(self):
+        as_dict = self.model_dump()
+        partition_key = f"{as_dict.pop("user_id")}{DELIMITER}{as_dict.pop("object_type")}"
+        as_dict[DB_PARTITION] = partition_key
+
+        return as_dict
+    
+    @classmethod
+    def from_db(cls, object: dict[str, str]):
+        try:
+            user_id, object_type = object.pop(DB_PARTITION).split(DELIMITER)
+        except ValueError:
+            raise ValueError(f"Invalid Partition: Object partition ({DB_PARTITION}) did not contain delimiter: {DELIMITER}")
+        except KeyError:
+            raise KeyError(f"Missing Partition: Object did not contain partition key ({DB_PARTITION})")
+
+        object.update({
+            "user_id": user_id,
+            "object_type": object_type
+        })
+
+        return cls.model_validate(object)
+
+
