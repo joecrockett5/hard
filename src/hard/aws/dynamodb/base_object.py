0d1be8cb48fce0a01bdb6772e1c5297e9e15dfff
joecrockett5 <joe@crockett.net>
2024-07-05 16:24:00 +0100

    Fix syntax error in `base_object.py`

--- src/hard/aws/dynamodb/base_object.py
+++ src/hard/aws/dynamodb/base_object.py
@@ -10,8 +10,10 @@ DELIMITER = "_"
 
 PARTITION_TEMPLATE = "{user_id}" + DELIMITER + "{object_type}"
 
+
 class BaseObject(BaseModel):
     """Base object for all items stored in DynamoDB"""
+
     user_id: str
     timestamp: datetime
     object_type: ObjectType
@@ -22,11 +24,11 @@ class BaseObject(BaseModel):
     @classmethod
     def parse_iso_format(cls, iso_date: str) -> datetime:
         return date_parser.isoparse(iso_date)
-    
+
     @field_serializer("timestamp")
     def convert_to_iso(timestamp: datetime):
         return timestamp.isoformat()
-    
+
     # `object_type` handling
     @field_validator("object_type")
     @classmethod
@@ -36,26 +38,29 @@ class BaseObject(BaseModel):
     @field_serializer("object_type")
     def get_value(object_type: ObjectType):
         return object_type.value
-    
+
     def to_db(self):
         as_dict = self.model_dump()
-        partition_key = f"{as_dict.pop("user_id")}{DELIMITER}{as_dict.pop("object_type")}"
+        partition_key = (
+            f'{as_dict.pop("user_id")}{DELIMITER}{as_dict.pop("object_type")}'
+        )
         as_dict[DB_PARTITION] = partition_key
 
         return as_dict
-    
+
     @classmethod
     def from_db(cls, object: dict[str, str]):
         try:
             user_id, object_type = object.pop(DB_PARTITION).split(DELIMITER)
         except ValueError:
-            raise ValueError(f"Invalid Partition: Object partition ({DB_PARTITION}) did not contain delimiter: {DELIMITER}")
+            raise ValueError(
+                f"Invalid Partition: Object partition ({DB_PARTITION}) did not contain delimiter: {DELIMITER}"
+            )
         except KeyError:
-            raise KeyError(f"Missing Partition: Object did not contain partition key ({DB_PARTITION})")
+            raise KeyError(
+                f"Missing Partition: Object did not contain partition key ({DB_PARTITION})"
+            )
 
-        object.update({
-            "user_id": user_id,
-            "object_type": object_type
-        })
+        object.update({"user_id": user_id, "object_type": object_type})
 
         return cls.model_validate(object)
