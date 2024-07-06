6caf623ad5af99b8c5f3d2a711650b651b2a73cc
joecrockett5 <joe@crockett.net>
2024-07-06 15:51:56 +0100

    Make `notes` optional

--- src/hard/models/workout.py
+++ src/hard/models/workout.py
@@ -1,4 +1,5 @@
 from datetime import date
+from typing import Optional
 
 from hard.aws.dynamodb.base_object import BaseObject
 from hard.aws.dynamodb.object_type import ObjectType
@@ -7,4 +8,4 @@ from hard.aws.dynamodb.object_type import ObjectType
 class Workout(BaseObject):
     object_type: ObjectType = ObjectType.WORKOUT
     date: date
-    notes: str
+    notes: Optional[str] = None
