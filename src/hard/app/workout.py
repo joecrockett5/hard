682599888452cfc15f7edd4d0fa613b182dc2a57
joecrockett5 <joe@crockett.net>
2024-07-06 11:19:30 +0100

    Update workout for less validation
    temporary whilst I figure out the api

--- src/hard/app/workout.py
+++ src/hard/app/workout.py
@@ -11,31 +11,31 @@ router = APIRouter(prefix="/workouts")
 # WORKOUT_PARTITION = PARTITION_TEMPLATE.format({"object_type": ObjectType.WORKOUT.value})
 
 
-@router.get("/", response_model=list[Workout])
-def list_workouts(req: Request) -> list[Workout]:
+@router.get("")
+def list_workouts(req: Request) -> Workout:
     user = request.get_user_claims(req)
     return {"email": user.email}
 
 
 @router.get("/{workout_id}", response_model=Workout)
-def get_workout(req: Request, workout_id: str) -> Workout:
+def get_workout(req: Request, workout_id: str):
     user = request.get_user_claims(req)
     pass
 
 
-@router.post("/", response_model=Workout)
-def create_workout(req: Request) -> Workout:
+@router.post("")
+def create_workout(req: Request):
     user = request.get_user_claims(req)
     pass
 
 
-@router.put("/{workout_id}", response_model=Workout)
-def update_workout(req: Request, workout_id: str) -> Workout:
+@router.put("/{workout_id}")
+def update_workout(req: Request, workout_id: str):
     user = request.get_user_claims(req)
     pass
 
 
-@router.delete("/{workout_id}", response_model=Workout)
-def delete_workout(req: Request, workout_id: str) -> Workout:
+@router.delete("/{workout_id}")
+def delete_workout(req: Request, workout_id: str):
     user = request.get_user_claims(req)
     pass
