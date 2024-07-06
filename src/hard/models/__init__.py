1cfe78255ee8fc794baaa03ca867f673c619b293
joecrockett5 <joe@crockett.net>
2024-07-06 20:03:02 +0100

    Update models

--- src/hard/models/__init__.py
+++ src/hard/models/__init__.py
@@ -4,11 +4,3 @@ from enum import Enum
 class WeightUnit(Enum):
     KILOGRAMS = "kgs"
     POUNDS = "lbs"
-
-
-from .exercise import Exercise
-from .exercise_join import ExerciseJoin
-from .set import Set
-from .tag import Tag
-from .tag_join import TagJoin
-from .workout import Workout
--- src/hard/models/workout.py
+++ src/hard/models/workout.py
@@ -1,6 +1,8 @@
 from datetime import date
 from typing import Optional
 
+from pydantic import field_serializer, field_validator
+
 from hard.aws.dynamodb.base_object import BaseObject
 from hard.aws.dynamodb.object_type import ObjectType
 
@@ -9,3 +11,12 @@ class Workout(BaseObject):
     object_type: ObjectType = ObjectType.WORKOUT
     date: date
     notes: Optional[str] = None
+
+    @field_serializer("date")
+    def to_isoformat(self, date: date) -> str:
+        return date.to_isoformat()
+
+    @field_validator("date")
+    @classmethod
+    def from_isoformat(cls, date_str: str) -> date:
+        return date.fromisoformat(date_str)
