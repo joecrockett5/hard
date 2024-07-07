c72e46994ddf64c4ffaa6f7f764740efcfebfe2e
joecrockett5 <joe@crockett.net>
2024-07-07 16:59:49 +0100

    add processing to endpoints

--- src/hard/app/workout/routes.py
+++ src/hard/app/workout/routes.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 from starlette.requests import Request
 
-from hard.aws.dynamodb.object_type import ObjectType
+from hard.app.workout import processes
+from hard.aws.dynamodb.consts import ItemAlreadyExistsError, ItemNotFoundError
 from hard.aws.interfaces.fastapi import request
 from hard.models.workout import Workout
 
@@ -10,31 +11,50 @@ router = APIRouter(prefix="/workouts")
 # WORKOUT_PARTITION = PARTITION_TEMPLATE.format({"object_type": ObjectType.WORKOUT.value})
 
 
-@router.get("")
-def list_workouts(req: Request) -> Workout:
+@router.get("", response_model=list[Workout])
+def get_list(req: Request) -> list[Workout]:
     user = request.get_user_claims(req)
-    return {"email": user.email}
+    return processes.list_workouts(user)
 
 
 @router.get("/{workout_id}", response_model=Workout)
-def get_workout(req: Request, workout_id: str):
-    user = request.get_user_claims(req)
-    pass
+def get(workout_id: str):
+    try:
+        workout = processes.get_workout(workout_id)
+    except ItemNotFoundError as err:
+        return {"error": err}, 404
+
+    return workout
 
 
-@router.post("")
-def create_workout(req: Request):
-    user = request.get_user_claims(req)
-    pass
+@router.post("", response_model=Workout)
+def post(workout: Workout):
+    try:
+        processes.create_workout(workout)
+    except ItemAlreadyExistsError as err:
+        return {"error": err}, 409
+
+    return workout
 
 
-@router.put("/{workout_id}")
-def update_workout(req: Request, workout_id: str):
-    user = request.get_user_claims(req)
-    pass
+@router.put("/{workout_id}", response_model=Workout)
+def put(workout_id: str, workout: Workout):
+    if workout.object_id != workout_id:
+        return {"error": "`Workout.object_id` cannot be changed"}, 400
+
+    try:
+        processes.update_workout(workout)
+    except ItemNotFoundError as err:
+        return {"error": err}, 404
+
+    return workout
 
 
-@router.delete("/{workout_id}")
-def delete_workout(req: Request, workout_id: str):
-    user = request.get_user_claims(req)
-    pass
+@router.delete("/{workout_id}", response_model=Workout)
+def delete(workout_id: str):
+    try:
+        deleted_workout = processes.delete_workout(workout_id)
+    except ItemNotFoundError as err:
+        return {"error": err}, 404
+
+    return deleted_workout
