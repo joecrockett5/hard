58373fe556311958f844491652811dfe0b790581
joecrockett5 <joe@crockett.net>
2024-07-07 07:47:26 +0100

    Add item index to db `consts`

--- src/hard/aws/dynamodb/consts.py
+++ src/hard/aws/dynamodb/consts.py
@@ -1,5 +1,9 @@
+import os
+
 DB_PARTITION = "User_ObjectType"
 DB_SORT_KEY = "Timestamp"
 DELIMITER = "#"
+
+ITEM_INDEX_NAME = os.getenv("DYNAMO_ITEM_INDEX_NAME")
 
 PARTITION_TEMPLATE = "{user_id}" + DELIMITER + "{object_type}"
