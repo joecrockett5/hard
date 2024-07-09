f6096eb77317981bd2abc3d5b2ea10eabaacfebf
joecrockett5 <joe@crockett.net>
2024-07-09 09:53:42 +0100

    Move `RestProcesses` to `staticmethod`s

--- src/hard/app/processes.py
+++ src/hard/app/processes.py
@@ -4,20 +4,41 @@ from hard.aws.models.user import User
 
 class RestProcesses:
 
-    def __init__(self, object_class: DB_OBJECT_TYPE) -> None:
-        self.object_class = object_class
-
-    def get_list(self, user: User) -> list[DB_OBJECT_TYPE]:
+    @staticmethod
+    def get_list(
+        object_cls: DB_OBJECT_TYPE,
+        user: User,
+    ) -> list[DB_OBJECT_TYPE]:
         pass
 
-    def get(self, user: User, object_id: str) -> DB_OBJECT_TYPE:
+    @staticmethod
+    def get(
+        object_cls: DB_OBJECT_TYPE,
+        user: User,
+        object_id: str,
+    ) -> DB_OBJECT_TYPE:
         pass
 
-    def post(self, user: User, object: DB_OBJECT_TYPE) -> DB_OBJECT_TYPE:
+    @staticmethod
+    def post(
+        object_cls: DB_OBJECT_TYPE,
+        user: User,
+        object: DB_OBJECT_TYPE,
+    ) -> DB_OBJECT_TYPE:
         pass
 
-    def put(self, user: User, object: DB_OBJECT_TYPE) -> DB_OBJECT_TYPE:
+    @staticmethod
+    def put(
+        object_cls: DB_OBJECT_TYPE,
+        user: User,
+        object: DB_OBJECT_TYPE,
+    ) -> DB_OBJECT_TYPE:
         pass
 
-    def delete(self, user: User, object_id: str) -> DB_OBJECT_TYPE:
+    @staticmethod
+    def delete(
+        object_cls: DB_OBJECT_TYPE,
+        user: User,
+        object_id: str,
+    ) -> DB_OBJECT_TYPE:
         pass
