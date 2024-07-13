3ce62728e46d072d237f193e7eb5cd0053aa5cfb
joecrockett5 <joe@crockett.net>
2024-07-13 18:07:00 +0100

    Add `target_object_type`

--- src/hard/models/tag_join.py
+++ src/hard/models/tag_join.py
@@ -8,6 +8,7 @@ from hard.aws.dynamodb.object_type import ObjectType
 
 class TagJoin(BaseObject):
     object_type: ObjectType = ObjectType.TAG_JOIN
+    target_object_type: ObjectType
     target_id: UUID
     tag_id: UUID
 
@@ -16,6 +17,11 @@ class TagJoin(BaseObject):
     def target_id_to_str(self, target_id: UUID) -> str:
         return str(target_id)
 
+    # tag_id handler
     @field_serializer("tag_id")
     def tag_id_to_str(self, tag_id: UUID) -> str:
         return str(tag_id)
+
+    @field_serializer("target_object_type")
+    def target_object_type_to_str(self, target_object_type: ObjectType) -> str:
+        return target_object_type.value
